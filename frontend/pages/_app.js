bd977bac1c6465310945b4d3c65169c5fee967d6
Beki <bereket1terefe@gmail.com>
2026-09-12 21:33:06 +0300

    update page title

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Img2Excel</title>
+        <title>Image to Excel</title>
         <link rel="icon" href="data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 100 100%22><text y=%22.9em%22 font-size=%2290%22>🖼️</text></svg>" />
       </Head>
       <Component {...pageProps} />
